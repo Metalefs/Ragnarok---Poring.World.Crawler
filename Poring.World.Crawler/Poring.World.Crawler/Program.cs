e9d12e4d0e73de0feec9fac72805433463f26096
Jackson Pires Ramalho <jack-ten@hotmail.com>
2020-02-19 23:55:06 -0300

    Update Program.cs

--- Poring.World.Crawler/Poring.World.Crawler/Program.cs
+++ Poring.World.Crawler/Poring.World.Crawler/Program.cs
@@ -6,10 +6,10 @@ namespace Main
 {
     class Program
     {
-        private readonly PoringCrawler CarCrawler = new PoringCrawler();
+        private readonly PoringCrawler Crawler = new PoringCrawler();
         static void Main(string[] args)
         {
-            Thread Crawler1 = new Thread(new ThreadStart(PoringCrawler.Start));
+            Thread Crawler1 = new Thread(new ThreadStart(Crawler.Start));
             Crawler1.Name = "First";
             Crawler1.Start();
             Console.Read();
