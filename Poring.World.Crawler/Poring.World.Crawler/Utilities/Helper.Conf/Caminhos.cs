9e2e0cd94562565f649f8050d7f86b9c4842e459
Jackson Pires Ramalho <jack-ten@hotmail.com>
2020-02-20 00:02:25 -0300

    Update Caminhos.cs

--- Poring.World.Crawler/Poring.World.Crawler/Utilities/Helper.Conf/Caminhos.cs
+++ Poring.World.Crawler/Poring.World.Crawler/Utilities/Helper.Conf/Caminhos.cs
@@ -8,10 +8,8 @@ namespace Utilities
 {
     public static class Caminhos
     {
-        public static string CaminhoLog = @"C:\Users\jackson.ramalho\source\repos\Poring.World.Crawler\Logs.txt";
-        public static string CaminhoItens = @"C:\Users\jackson.ramalho\source\repos\Poring.World.Crawler\Itens.txt";
-        public static string URL_Rumo_Crawler = "https://rumosolucoes.com/";
+        public static string CaminhoLog = @"Logs.txt";
+        public static string CaminhoItens = @"Itens.txt";
         public static string URL_Poring_Crawler = "https://www.poring.world/";
-        public static string URL_CNDT = "http://servicos.receita.fazenda.gov.br/Servicos/certidao/CNDConjuntaInter/InformaNICertidao.asp?tipo=1&NI=06198864000146&txtTexto_captcha_serpro_gov_br";
     }
 }
